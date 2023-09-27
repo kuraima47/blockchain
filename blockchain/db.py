07b92a220718251e15ce213526471ab519a48d4f
kuraima47 <tpottier47@gmail.com>
2023-09-27 10:51:07 +0200

    db request

--- blockchain/db.py
+++ blockchain/db.py
@@ -4,7 +4,61 @@ import utils
 
 
 class Db:
+
     def __init__(self, filename, recover=False):
+        self.conn, self.c = None, None
+        self.tables = [
+            {
+                "name": "eoa_accounts",
+                "fields": [
+                    ("address", "TEXT"),
+                    ("balance", "INTEGER"),
+                    ("nonce", "INTEGER"),
+                ],
+            },
+            {
+                "name": "accounts",
+                "fields": [
+                    ("address", "TEXT"),
+                    ("balance", "INTEGER"),
+                    ("nonce", "INTEGER"),
+                    ("code", "TEXT"),
+                    ("storage", "TEXT"),
+                ],
+            },
+            {
+                "name": "contract_accounts",
+                "fields": [
+                    ("address", "TEXT"),
+                    ("balance", "INTEGER"),
+                    ("nonce", "INTEGER"),
+                    ("storage", "TEXT"),
+                ],
+            },
+            {
+                "name": "contract_storage",
+                "fields": [
+                    ("address", "TEXT"),
+                    ("key", "TEXT"),
+                    ("value", "TEXT"),
+                ],
+            },
+            {
+                "name": "tx_pool",
+                "fields": [
+                    ("hash", "TEXT"),
+                    ("nonce", "INTEGER"),
+                    ("gas_price", "INTEGER"),
+                    ("gas", "INTEGER"),
+                    ("to", "TEXT"),
+                    ("value", "INTEGER"),
+                    ("data", "TEXT"),
+                    ("v", "INTEGER"),
+                    ("r", "INTEGER"),
+                    ("s", "INTEGER"),
+                ],
+            },
+        ]
         self.filename = filename
         if recover:
             self.load()
@@ -28,26 +82,34 @@ class Db:
         self.conn.close()
 
     def create_all_tables(self):
-        pass
+        for table in self.tables:
+            self.create_table(table["name"], **dict(table["fields"]))
 
     def drop_all_tables(self):
-        pass
+        for table in self.tables:
+            self.drop_table(table["name"])
 
     def create_table(self, table_name, **fields):
-        # create a table in the db
-        pass
+        self.c.execute(
+            f"CREATE TABLE {table_name} ({','.join([f'{k} {v}' for k, v in fields.items()])})"
+        )
+        self.conn.commit()
 
     def drop_table(self, table_name):
-        # drop a table from the db
-        pass
+        self.c.execute(f"DROP TABLE {table_name}")
+        self.conn.commit()
 
     def insert(self, table_name, **values):
-        # insert values into a table
-        pass
+        self.c.execute(
+            f"INSERT INTO {table_name} ({','.join([k for k in values.keys()])}) VALUES ({','.join(['?' for _ in values.keys()])})",
+            [v for v in values.values()],
+        )
 
     def update(self, table_name, **values):
-        # update contract into table
-        pass
+        self.c.execute(
+            f"UPDATE {table_name} SET {','.join([f'{k}=?' for k in values.keys()])}",
+            [v for v in values.values()],
+        )
 
     @property
     def hash(self):
